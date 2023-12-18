63397a60d6a0725f05c7425027fce8d8fc67fbfd
ChrisOrdi <chris.vandalen@ordina.nl>
2023-12-18 15:47:34 +0100

    folder structure updated.

---
+++ src/main/java/com/sneakercity/sneakerstore/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.sneakercity.sneakerstore.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+}
---
+++ src/main/java/com/sneakercity/sneakerstore/service/SneakerService.java
@@ -0,0 +1,7 @@
+package com.sneakercity.sneakerstore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SneakerService {
+}
