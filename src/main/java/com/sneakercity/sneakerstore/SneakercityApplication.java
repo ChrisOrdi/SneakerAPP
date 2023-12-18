d3950041b764810c8393fbfd615135123e21ddff
ChrisOrdi <chris.vandalen@ordina.nl>
2023-12-18 15:26:32 +0100

    Initial commit

---
+++ src/main/java/com/sneakercity/sneakerstore/SneakercityApplication.java
@@ -0,0 +1,13 @@
+package com.sneakercity.sneakerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakercityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakercityApplication.class, args);
+    }
+
+}
