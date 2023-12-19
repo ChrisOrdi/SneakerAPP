f368c03b6319ce25c6548e24a0fe7bd254dee772
ChrisOrdi <chris.vandalen@ordina.nl>
2023-12-19 10:04:31 +0100

    Addded getAllSneakers functionality

--- src/main/java/com/sneakercity/sneakerstore/service/SneakerService.java
+++ src/main/java/com/sneakercity/sneakerstore/service/SneakerService.java
@@ -1,7 +1,21 @@
 package com.sneakercity.sneakerstore.service;
 
+import com.sneakercity.sneakerstore.model.Sneaker;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SneakerService {
+
+    private final List<Sneaker> sneakerList = new ArrayList<>();
+
+    // Return every sneaker / entry
+    public List<Sneaker> getAllSneakers() {
+         return new ArrayList<>(sneakerList);
+    }
+
+
+
 }
